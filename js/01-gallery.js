e804bc0b41a1aab039562d2964fb6a275e995655
anasty223 <anasty22@gmail.com>
2021-11-12 15:55:40 +0200

    заминка
    как достучаться к url большого изображения?

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// console.log(createImagesCard(galleryItems));
+
+const imageContainer = document.querySelector('.gallery');
+const cardsMarkup = createImagesCard(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend',cardsMarkup)
+
+imageContainer.addEventListener('click',onContainerClick)
+
+function createImagesCard(galleryItems) {
+    
+    return galleryItems.map(({ preview, original, description }) => {
+       
+        return `
+     
+<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+    }).join('');
+ 
+}
+
+function onContainerClick(evt) {
+    evt.preventDefault();
+    
+const isGalleryImage = evt.target.classList.contains('gallery__image')
+    if (!isGalleryImage) {
+        return;
+    }
+    const imageGal = evt.target;
+    console.log(imageGal);
+
+}
